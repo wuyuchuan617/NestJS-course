8ea4f1867aa41b062fdf149da124141bf4cf1715
wuyuchuan617 <jenniferwu617@gmail.com>
2023-07-13 22:56:12 +0800

    fix description

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,15 +1,12 @@
-import classes from './MeetupDetail.module.css';
+import classes from "./MeetupDetail.module.css";
 
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-      <img
-        src={props.image}
-        alt={props.title}
-      />
+      <img src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
-      <p>{props.description}</p>
+      <p>{`${props.description}`}</p>
     </section>
   );
 }
