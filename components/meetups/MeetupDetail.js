731c9ffed7bcd3e9be1076eaf7fde6370c5b5e3c
wuyuchuan617 <jenniferwu617@gmail.com>
2023-07-13 23:02:42 +0800

    fix description

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,12 +1,15 @@
 import classes from "./MeetupDetail.module.css";
 
 function MeetupDetail(props) {
+  const descriptionLines = props.description.split("\n");
   return (
     <section className={classes.detail}>
       <img src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
-      {`${props.description}`}
+      {descriptionLines.map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
     </section>
   );
 }
