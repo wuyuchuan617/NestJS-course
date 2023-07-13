6f4d2617c2488f7fe423f827f76e573e9ed29b20
wuyuchuan617 <jenniferwu617@gmail.com>
2023-07-14 00:41:42 +0800

    fix reverse

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export async function getStaticProps() {
   const db = client.db();
 
   const meetupsCollection = db.collection("meetups");
-  const meetups = (await meetupsCollection.find().toArray()).reverse();
+  const meetups = ((await meetupsCollection.find().toArray()).reverse());
 
   return {
     props: {
