5e7f8c80bb6928e3c009e2fdc584c5c175079413
wuyuchuan617 <jenniferwu617@gmail.com>
2023-07-13 23:36:36 +0800

    fix description

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -7,7 +7,14 @@ function MeetupDetail(props) {
       <img src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
-      <div style={{ textAlign: "left" , padding:'100px 25vw'}}>
+      <div
+        style={{
+          textAlign: "left",
+          padding: "100px 25vw",
+          color: "#807669",
+          lineHeight: "1.8",
+        }}
+      >
         {descriptionLines.map((line, index) => (
           <p key={index}>{line}</p>
         ))}
